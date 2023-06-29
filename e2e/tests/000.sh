b08740bc8c776d8bc8f4276e2d3c161660a247df
John Belamaric <jbelamaric@google.com>
2023-06-29 16:32:18 -0700

    Add script to change porch server build

---
+++ e2e/tests/000.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2023 The Nephio Authors.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+## TEST METADATA
+## TEST-NAME: Deploy regional workload cluster
+##
+
+set -o pipefail
+set -o errexit
+set -o nounset
+[[ ${DEBUG:-false} != "true" ]] || set -o xtrace
+
+export HOME=${HOME:-/home/ubuntu/}
+export E2EDIR=${E2EDIR:-$HOME/test-infra/e2e}
+export TESTDIR=${TESTDIR:-$E2EDIR/tests}
+export LIBDIR=${LIBDIR:-$E2EDIR/lib}
+
+source "${LIBDIR}/k8s.sh"
+
+kubeconfig="$HOME/.kube/config"
+
+porch=/tmp/porch-dev
+kpt fn eval --image "gcr.io/kpt-fn/set-image:v0.1.1" $porch -- name=nephio/porch-server newTag=jbelamaric
+kpt live apply $porch
